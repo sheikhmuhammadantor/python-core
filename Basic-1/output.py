e2fb26471816f6bfce71ac2ea8aa4018d612e7c4
Sheikh Muhammad Antor <sheikhmuhammadantor@gmail.com>
2025-03-25 14:48:43 +0600

    Add output.py for Basic Python 1 examples.

---
+++ Basic-1/output.py
@@ -0,0 +1,27 @@
+# Basic Python 1 - Output
+# 1. print() function
+
+print('Hello World 1 !')
+print('Hello World 2 !')
+
+print('Hello World 3 !', end=' ')
+print('- Hello World 4 !')
+
+print('Hello World 3 !','- Hello World 4 !')
+
+print(5)
+print(5.0)
+
+print('5')
+print('5.0')
+
+print(5 + 5)
+print(5.0 + 5.0)
+print('5' + '5')
+# print('5.0' + 5) # TypeError: can only concatenate str (not "int") to str
+
+print(5 * 5); 
+print(5 / 5); 
+print(5 - 5); 
+print(5 % 5); 
+print(5 ** 3)
