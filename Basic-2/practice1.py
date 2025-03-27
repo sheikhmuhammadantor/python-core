00a3f287fa7d0d186e734f565248658dd8f08a4a
Sheikh Muhammad Antor <sheikhmuhammadantor@gmail.com>
2025-03-27 22:23:28 +0600

    Add practice examples for string operations and input handling in Python.

---
+++ Basic-2/practice1.py
@@ -0,0 +1,7 @@
+# Practice 1:
+fist_name = str(input("Enter Your Fist Name: "));
+print("The length of your fist name :", len(fist_name));
+
+# Practice 2:
+name = "A Hi, A I Am Antor, A brother of Abir";
+print("The Count of A is :", name.count("A"));
---
+++ Basic-2/strings.py
@@ -0,0 +1,44 @@
+# 3. String:
+name_single_quotes = 'Muhammad';
+name_double_quotes = "Muhammad";
+name_triple_quotes = '''Muhammad''';
+name_triple_double_quotes = """Muhammad""";
+
+print(name_single_quotes, name_double_quotes, name_triple_quotes, name_triple_double_quotes);
+
+# Escape Sequence Characters:
+str1 = "This is a String.\n\tWe are creating it in Python!"
+print(str1);
+
+# Concatenation:
+str2 = "Hello";
+str3 = "World";
+print(str2 + " " + str3,"!");
+
+# Length:
+final_str = (str2 + " " + str3);
+print(len(final_str));
+
+# Indexing:
+name = "Sheikh Muhammad Antor !";
+ch = name[5];
+print(ch);
+
+# Slicing:
+sl = name[7:15];
+Nsl = name[-16:-8];
+print(sl);
+print(Nsl);
+
+# M  u  h  a  m  m  a  d
+# 0  1  2  3  4  5  6  7 
+#-8 -7 -6 -5 -4 -3 -2 -1
+
+# String Functions: ...more;
+# Same as JavaScript - String Methods;
+"""
+1. String.endswith("a");
+2. String.capitalize();
+3. String.replace("I", "u");
+4. String.find("a")
+"""
