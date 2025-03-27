1173c7018933c232291332a80f81de37ab61dcf8
Sheikh Muhammad Antor <sheikhmuhammadantor@gmail.com>
2025-03-27 21:37:40 +0600

    Add input handling and operator examples in Python.

---
+++ Basic-1/input.py
@@ -0,0 +1,9 @@
+# Taking input from user and printing it
+name = input("Enter your name: ");
+print("Hello", name);
+
+age = int(input("Enter your age: "));
+print("Your age is", age);
+
+price = float(input("Enter the price: "));
+print("The price is", price);
---
+++ Basic-1/operators.py
@@ -0,0 +1,14 @@
+# Types of Operators:
+# Same as JavaScript!
+# 1. Arithmetic Operators (+, -, *, /, %, **)
+# 2. Relational / Comparison Operators (==, !=, >, <, >=, <=)
+# 3. Assignment Operators (=, +=, -=, *=, /=, %=, **=)
+
+# Like JavaScript!
+# 4. Logical Operators:
+"""
+    not like !
+    and like &&
+    or like ||
+"""
+# 5. 
